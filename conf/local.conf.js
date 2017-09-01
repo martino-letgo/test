ec7793683341f81e9b4fd60518495ddb3c5839c9
martino-letgo <30622772+martino-letgo@users.noreply.github.com>
2017-09-01 12:24:32 +0200

    Change log to error

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -89,7 +89,7 @@ exports.config = {
     //
     // Level of logging verbosity: silent | verbose | command | data | result |
     // error
-    logLevel: 'verbose',
+    logLevel: 'error',
     //
     // Enables colors for log output.
     coloredLogs: true,
